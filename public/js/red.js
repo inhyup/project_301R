8b837d66b6c4b270fdaf2b955fc60c3a83ab31ac
inhyup <hyup3569@gmail.com>
2017-12-13 21:29:57 -0700

    restore

--- public/js/red.js
+++ public/js/red.js
@@ -56,9 +56,6 @@ var modalUnderlay = document.querySelector('.modal_underlay');
 var modalBtn = document.querySelector('.modal .btn');
 var loader = document.querySelector('.loader');
 
-var contactBtn = document.querySelector('.contact .contact_btn');
-contactBtn.addEventListener('click', checkFields);
-
 modalBtn.addEventListener('click', checkFields);
 
 
